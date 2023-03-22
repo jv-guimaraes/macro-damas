c79db428fd4a04e86bccad35cf905c1d6f9d56b3
jv-guimaraes <joao.guimaraes@aluno.uepb.edu.br>
2023-03-22 19:39:34 -0300

    Desenhar tabuleiro e peças

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,22 @@ use macroquad::prelude::*;
 mod tabuleiro;
 use tabuleiro::Tabuleiro;
 
+const TABULEIRO: [[char; 8]; 8] = [
+    ['.', 'p', '.', 'p', '.', 'p', '.', 'p'],
+    ['p', '.', 'p', '.', 'p', '.', 'p', '.'],
+    ['.', 'p', '.', '.', '.', 'p', '.', 'p'],
+    ['.', '.', '.', '.', 'p', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', 'b', '.', '.'],
+    ['b', '.', 'b', '.', '.', '.', 'b', '.'],
+    ['.', 'b', '.', 'b', '.', 'b', '.', 'b'],
+    ['b', '.', 'b', '.', 'b', '.', 'b', '.'],
+];
+
 fn window_conf() -> Conf {
     Conf {
         window_title: "Macro Damas".to_owned(),
-        window_width: 550,
-        window_height: 550,
+        window_width: 600,
+        window_height: 600,
         ..Default::default()
     }
 }
@@ -16,10 +27,9 @@ fn window_conf() -> Conf {
 #[macroquad::main(window_conf)]
 async fn main() {
     let tabuleiro = Tabuleiro::new().await;
-    let jogo = damas::Jogo::default();
-    println!("{jogo}");
+    let jogo = damas::Jogo::new(TABULEIRO);
     loop {
-        tabuleiro.draw();
+        tabuleiro.draw(jogo.get_tabuleiro());
         next_frame().await
     }
 }
--- src/tabuleiro.rs
+++ src/tabuleiro.rs
@@ -1,17 +1,27 @@
+use damas::Casa;
 use macroquad::prelude::*;
 
 pub struct Tabuleiro {
     textura: Texture2D,
+    branca: Texture2D,
+    preta: Texture2D,
 }
 
 impl Tabuleiro {
     pub async fn new() -> Tabuleiro {
         Tabuleiro {
-            textura: load_texture("assets\\tabuleiro.png").await.unwrap()
+            textura: load_texture("assets\\tabuleiro.png").await.unwrap(),
+            branca: load_texture("assets\\pedra_branca.png").await.unwrap(),
+            preta: load_texture("assets\\pedra_preta.png").await.unwrap(),
         }
     }
 
-    pub fn draw(&self) {
+    pub fn draw(&self, tabuleiro: &[[Casa; 8]; 8]) {
+        self.desenhar_tabuleiro();
+        self.desenhar_pedras(tabuleiro);
+    }
+
+    fn desenhar_tabuleiro(&self) {
         draw_texture_ex(
             self.textura,
             0.0,
@@ -22,5 +32,32 @@ impl Tabuleiro {
                 ..Default::default()
             },
         );
+    }
+
+    fn desenhar_pedras(&self, tabuleiro: &[[Casa; 8]; 8]) {
+        let offset_h = screen_width() * 0.032407;
+        let offset_v = screen_height() * 0.032407;
+        let width = (screen_width() - (offset_h * 2.0)) / 8.0;
+        let height = (screen_height() - (offset_v * 2.0)) / 8.0;
+        #[allow(clippy::needless_range_loop)]
+        for y in 0..8 {
+            for x in 0..8 {
+                let casa = tabuleiro[y][x];
+                if casa.é_vazia() {
+                    continue;
+                }
+                let peça = casa.peça().unwrap();
+                draw_texture_ex(
+                    if peça.é_preta() { self.preta } else { self.branca },
+                    x as f32 * width + offset_h,
+                    y as f32 * height + offset_v,
+                    WHITE,
+                    DrawTextureParams {
+                        dest_size: Some(vec2(width, height)),
+                        ..Default::default()
+                    },
+                );
+            }
+        }
     }
 }
